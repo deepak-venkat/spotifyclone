32fd60fd464dfcc720a6c8d5939f898acf0d9def
deepak <deepakvenkat006@gmail.com>
2024-08-24 16:08:02 +0530

    small changes

--- src/components/AudioPlayer/index.js
+++ src/components/AudioPlayer/index.js
@@ -1,6 +1,5 @@
 import { Player } from "./styledComponents";
 import "react-h5-audio-player/lib/styles.css";
-import "./index.css";
 
 const AudioPlayer = ({ audioSrc }) => (
   <Player src={audioSrc} autoPlay controls />
